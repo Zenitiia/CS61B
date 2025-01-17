c317bdf622ae7595a7cda1e80b5cd3ff4dfe2900
irota <1587243508@qq.com>
2025-01-17 20:54:05 +0800

    2025.1.17 upd

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+
+public class TestArrayDequeGold {
+    @Test
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> test = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> std = new ArrayDequeSolution<>();
+        StringBuffer log = new StringBuffer();
+        for (int i = 0; i < 114; i++) {
+            Integer x = StdRandom.uniform(114514);
+            Integer type = StdRandom.uniform(4);
+            Integer removedStd = 0;
+            Integer removedTest = 0;
+            if (std.isEmpty()) {
+                type /= 2;
+            }
+            if (type == 0) {
+                log.append("addFirst(");
+                log.append(x);
+                log.append(")\n");
+                test.addFirst(x);
+                std.addFirst(x);
+            } else if (type == 1) {
+                log.append("addLast(");
+                log.append(x);
+                log.append(")\n");
+                test.addLast(x);
+                std.addLast(x);
+            } else if (type == 2) {
+                log.append("removeFirst()");
+                removedTest = test.removeFirst();
+                removedStd = std.removeFirst();
+            } else {
+                log.append("removeLast()");
+                removedTest = test.removeLast();
+                removedStd = std.removeLast();
+            }
+            assertEquals(log.toString(), removedTest, removedStd);
+        }
+    }
+}
